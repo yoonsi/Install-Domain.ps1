2b4b21abe2a5b5c7b45133ad33c3fcae34306e3e
Keshan Pillay <sinfulsymphony@gmail.com>
2019-03-31 17:18:18 +1300

    Update Install-Domain.ps1

--- Install-Domain.ps1
+++ Install-Domain.ps1
@@ -1,4 +1,9 @@
-﻿Install-WindowsFeature AD-Domain-Services
+## Author - KP - Installation of Domain Services and Domain via Powershell
+
+$domainName = "yoonsi.com"
+$domainNetBiosName = "YOONSI"
+
+Install-WindowsFeature AD-Domain-Services
 
 Import-Module ADDSDeployment
 
@@ -6,8 +11,8 @@ Install-ADDSForest `
 -CreateDnsDelegation:$false `
 -DatabasePath “C:\Windows\NTDS” `
 -DomainMode “Win2012R2” `
--DomainName “yoonsi.com” `
--DomainNetbiosName “YOONSI” `
+-DomainName $domainName `
+-DomainNetbiosName $domainNetBiosName `
 -ForestMode “Win2012R2” `
 -InstallDns:$true `
 -LogPath “C:\Windows\NTDS” `
