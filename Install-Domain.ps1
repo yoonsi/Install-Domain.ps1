7822b0f19d255a3417080165d80a93d6fc79d440
Keshan Pillay <sinfulsymphony@gmail.com>
2019-03-31 17:14:02 +1300

    Add files via upload

---
+++ Install-Domain.ps1
@@ -0,0 +1,16 @@
+﻿Install-WindowsFeature AD-Domain-Services
+
+Import-Module ADDSDeployment
+
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath “C:\Windows\NTDS” `
+-DomainMode “Win2012R2” `
+-DomainName “yoonsi.com” `
+-DomainNetbiosName “YOONSI” `
+-ForestMode “Win2012R2” `
+-InstallDns:$true `
+-LogPath “C:\Windows\NTDS” `
+-NoRebootOnCompletion:$false `
+-SysvolPath “C:\Windows\SYSVOL” `
+-Force:$true `
